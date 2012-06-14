e1598eceb76c73cf225d403039e93125381a7e96
Davide Ceneda <dceneda@yahoo.it>
2012-06-14 14:15:30 +0200

    aggiunti organuli cellulari

--- Progetto Finale/neurone.js
+++ Progetto Finale/neurone.js
@@ -81,8 +81,14 @@ var nucleo = function(){
 	var sphere1 = STRUCT([d9,d10,d11,d12]);
 	var disk = STRUCT([d1,d2,d3,d4]);
 
+	var quarterdisk1 = B([[0,-5,0],[0,-5,5],[0,0,5]]);
+	var d13= COLOR([1,165/255,79/255])(MAP(BEZIER(S1)([quarterdisk1,p0]))(dd));
+	var quarterdisk2   = B([[0,5,0],[0,5,5],[0,0,5]]);
+	var d14= COLOR([1,165/255,79/255])(MAP(BEZIER(S1)([quarterdisk2,p0]))(dd));
 
-	var model = STRUCT([sphere,sphere1,disk]);
+	var facciaFrontale = STRUCT([d13,d14]);
+
+	var model = STRUCT([sphere,sphere1,disk,facciaFrontale]);
 	
 	return model;
 
@@ -101,8 +107,8 @@ var cellula = function(){
 	var c3 = H([[0,-n,h],[-n,0,h],[-n*1.6568,0,0],[0,n*1.6568,0]]);
 	var c4 = H([[-n,0,h],[0,n,h],[0,n*1.6568,0],[n*1.6568,0,0]]);
 
-	h = 9;
-	n = 9.4;
+	h = 10;
+	n = 10;
 	var c5 = H([[0,n,h],[n,0,h],[n*1.6568,0,0],[0,-n*1.6568,0]]);
 	var c6 = H([[n,0,h],[0,-n,h],[0,-n*1.6568,0],[-n*1.6568,0,0]]);
 	var c7 = H([[0,-n,h],[-n,0,h],[-n*1.6568,0,0],[0,n*1.6568,0]]);
@@ -142,13 +148,20 @@ var cellula = function(){
 
 	var sphere = STRUCT([d5,d6,d7,d8]);
 	var sphere1 = STRUCT([d9,d10,d11,d12]);
-	var disk = STRUCT([d1,d2,d3,d4]);
+	var facciaBase = STRUCT([d1,d2,d3,d4]);
 
-	var disk2 = (R([1,2])(-PI/2)(disk));
-	disk2 = COLOR([1,165/255,79/255,1])(disk2); 
+	
+
+	var mezzodisco1   = B([[-10,0,0],[-10,0,10],[0,0,10]]);
+	var mezzodisco2   = B([[10,0,0],[10,0,10],[0,0,10]]);
+	var d13= COLOR(c)(MAP(BEZIER(S1)([mezzodisco1,p0]))(dd));
+	var d14= COLOR(c)(MAP(BEZIER(S1)([mezzodisco2,p0]))(dd));
+
+	var facciaFrontale = STRUCT([d13,d14]);
 
 
-	var model = STRUCT([sphere,sphere1,disk,disk2]);
+	var model = STRUCT([sphere,sphere1,facciaBase,facciaFrontale]);
+	
 	
 	return model;
 
@@ -201,15 +214,24 @@ var dendrite = function(){
 
 	var p0  = B([[0,0,30]]);
 
-	var c = [1,165/255,79/255];
-	var d1 = COLOR(c)(MAP(BEZIER(S1)([c1,c5,c9,c13,c17,p0]))(dd));
-	var d2 = COLOR(c)(MAP(BEZIER(S1)([c2,c6,c10,c14,c18,p0]))(dd));
-	var d3 = COLOR(c)(MAP(BEZIER(S1)([c3,c7,c11,c15,c19,p0]))(dd));
-	var d4 = COLOR(c)(MAP(BEZIER(S1)([c4,c8,c12,c16,c20,p0]))(dd));
 
-	var dendr = STRUCT([d1,d2,d3,d4]);
+	h = 30.0;
+	n = 0.2;
+
+	var c21 = H([[0,n,h],[n,0,h],[n*1.6568,0,0],[0,-n*1.6568,0]]);
+	var c22= H([[n,0,h],[0,-n,h],[0,-n*1.6568,0],[-n*1.6568,0,0]]);
+	var c23= H([[0,-n,h],[-n,0,h],[-n*1.6568,0,0],[0,n*1.6568,0]]);
+	var c24= H([[-n,0,h],[0,n,h],[0,n*1.6568,0],[n*1.6568,0,0]]);
+
+	var c = [1,165/255,79/255];
+	var d1 = COLOR(c)(MAP(BEZIER(S1)([c1,c5,c9,c13,c17,c21]))(dd));
+	var d2 = COLOR(c)(MAP(BEZIER(S1)([c2,c6,c10,c14,c18,c22]))(dd));
+	var d3 = COLOR(c)(MAP(BEZIER(S1)([c3,c7,c11,c15,c19,c23]))(dd));
+	var d4 = COLOR(c)(MAP(BEZIER(S1)([c4,c8,c12,c16,c20,c24]))(dd));
+
+	var model = STRUCT([d1,d2,d3,d4]);
 	
-	return dendr;
+	return model;
 
 }
 
@@ -267,14 +289,14 @@ var dendrite2 = function(){
 	var p0  = B([[0+5,0,30]]);
 
 	var c = [1,165/255,79/255];
-	var d1 = COLOR(c)(MAP(BEZIER(S1)([c1,c5,c9,c13,c17,c21,p0]))(dd));
-	var d2 = COLOR(c)(MAP(BEZIER(S1)([c2,c6,c10,c14,c18,c22,p0]))(dd));
-	var d3 = COLOR(c)(MAP(BEZIER(S1)([c3,c7,c11,c15,c19,c23,p0]))(dd));
-	var d4 = COLOR(c)(MAP(BEZIER(S1)([c4,c8,c12,c16,c20,c24,p0]))(dd));
+	var d1 = COLOR(c)(MAP(BEZIER(S1)([c1,c5,c9,c13,c17,c21]))(dd));
+	var d2 = COLOR(c)(MAP(BEZIER(S1)([c2,c6,c10,c14,c18,c22]))(dd));
+	var d3 = COLOR(c)(MAP(BEZIER(S1)([c3,c7,c11,c15,c19,c23]))(dd));
+	var d4 = COLOR(c)(MAP(BEZIER(S1)([c4,c8,c12,c16,c20,c24]))(dd));
 
-	var dendr = STRUCT([d1,d2,d3,d4]);
-	//DRAW(dendr);
-	return dendr;
+	var model = STRUCT([d1,d2,d3,d4]);
+	
+	return model;
 
 }
 
@@ -321,18 +343,26 @@ var dendrite1 = function(){
 	var c19= H([[0+0.5,-n,h],[-n+0.5,0,h],[-n*1.6568,0,0],[0,n*1.6568,0]]);
 	var c20= H([[-n+0.5,0,h],[0+0.5,n,h],[0,n*1.6568,0],[n*1.6568,0,0]]);
 
+	h = 30.0;
+	n = 0.2;
+
+	var c21 = H([[0,n,h],[n,0,h],[n*1.6568,0,0],[0,-n*1.6568,0]]);
+	var c22= H([[n,0,h],[0,-n,h],[0,-n*1.6568,0],[-n*1.6568,0,0]]);
+	var c23= H([[0,-n,h],[-n,0,h],[-n*1.6568,0,0],[0,n*1.6568,0]]);
+	var c24= H([[-n,0,h],[0,n,h],[0,n*1.6568,0],[n*1.6568,0,0]]);
+
 	var p0  = B([[3,0,30]]);
 
 	var c = [1,165/255,79/255];
-	var d1 = COLOR(c)(MAP(BEZIER(S1)([c1,c5,c9,c13,c17,p0]))(dd));
-	var d2 = COLOR(c)(MAP(BEZIER(S1)([c2,c6,c10,c14,c18,p0]))(dd));
-	var d3 = COLOR(c)(MAP(BEZIER(S1)([c3,c7,c11,c15,c19,p0]))(dd));
-	var d4 = COLOR(c)(MAP(BEZIER(S1)([c4,c8,c12,c16,c20,p0]))(dd));
+	var d1 = COLOR(c)(MAP(BEZIER(S1)([c1,c5,c9,c13,c17,c21]))(dd));
+	var d2 = COLOR(c)(MAP(BEZIER(S1)([c2,c6,c10,c14,c18,c22]))(dd));
+	var d3 = COLOR(c)(MAP(BEZIER(S1)([c3,c7,c11,c15,c19,c23]))(dd));
+	var d4 = COLOR(c)(MAP(BEZIER(S1)([c4,c8,c12,c16,c20,c24]))(dd));
 
-	var dendr = STRUCT([d1,d2,d3,d4]);
+	var model = STRUCT([d1,d2,d3,d4]);
 	
 	
-	return dendr;
+	return model;
 
 }
 
@@ -386,7 +416,8 @@ var assone = function(){
 
 	var d2 = COLOR(c)(MAP(BEZIER(S1)([cc1,cc2]))(dd));
 
-	return ( STRUCT([d1,d4,d2]));
+	var model = (STRUCT([d1,d4,d2]));
+	return model;
 	
 	
 	
@@ -438,8 +469,8 @@ var dendriteCompleto = function(){
 	d9 = R([0,1])(PI/2)(d9);
 	d9 = T([2])([14.5])(d9);
 
-	var dc = STRUCT([d1,d2,d3,d4,d5,d6,d7,d8,d9]);
-	return dc;
+	var model = STRUCT([d1,d2,d3,d4,d5,d6,d7,d8,d9]);
+	return model;
 }
 
 
@@ -487,8 +518,8 @@ var dendriteCompleto1 = function(){
 	d9 = R([0,1])(PI/2)(d9);
 	d9 = T([2])([14.5])(d9);
 
-	var dc = STRUCT([d1,d2,d3,d4,d5,d6,d7,d8,d9]);
-	return dc;
+	var model = STRUCT([d1,d2,d3,d4,d5,d6,d7,d8,d9]);
+	return model;
 }
 
 
@@ -555,15 +586,142 @@ var dendriti = function(){
 
 
 
-	var ddr = STRUCT([d1,d2,d3,d4,d5,d6,d7,d8]);
-	return ddr;
+	var model = STRUCT([d1,d2,d3,d4,d5,d6,d7,d8]);
+	return model;
 }
+
+
+var complessoGolgi = function(){
+	var quotes = [[1.5],[0.5],[0.2]];
+	var golgi = SIMPLEX_GRID(quotes);
+	golgi = T([0])([-0.75])(golgi);
+
+	quotes = [[2],[0.5],[0.2]];
+	var golgi1 = SIMPLEX_GRID(quotes);
+	golgi1 = T([0,2])([-1,0.3])(golgi1);
+
+	quotes = [[2.5],[0.5],[0.2]];
+	var golgi2 = SIMPLEX_GRID(quotes);
+	golgi2 = T([0,2])([-1.25,0.6])(golgi2);
+
+	quotes = [[3],[0.5],[0.2]];
+	var golgi3 = SIMPLEX_GRID(quotes);
+	golgi3 = T([0,2])([-1.5,0.9])(golgi3);
+
+	quotes = [[3.02],[0.5],[0.2]];
+	var golgi4 = SIMPLEX_GRID(quotes);
+	golgi4 = T([0,2])([-1.56,1.2])(golgi4);
+
+	quotes = [[2.5],[0.5],[0.2]];
+	var golgi5 = SIMPLEX_GRID(quotes);
+	golgi5 = T([0,2])([-1.25,1.5])(golgi5);
+	
+	var s = STRUCT([golgi,golgi1,golgi2,golgi3,golgi4,golgi5])
+	s = R([0,2])(PI/4)(s);
+	s = T([0,1,2])([5,-0.25,5])(s);
+	
+	var apparatoGolgi = COLOR([153/255,50/255,205/255])(s);
+	return apparatoGolgi;
+}
+
+var mitocondrio = function(){
+
+	var r = 0.4;
+	var n = 20;
+
+	var sphereDomain = DOMAIN([[0,PI],[0,2*PI]])([n,n]);
+	var sphere = MAP(function(p){
+		var u = p[0]-PI/2;
+		var v = p[1]-PI;
+		return [r*Math.cos(u)*Math.sin(v),r*Math.cos(u)*Math.cos(v),r*Math.sin(u)];
+	})(sphereDomain);
+
+	var mitocondrio = COLOR([115/255,0,0])(sphere);
+	mitocondrio = S([0])([1.5])(mitocondrio);
+	return mitocondrio;
+
+}
+
+var mitocondri = function(){
+	var mito1 = mitocondrio();
+	mito1 = R([0,2])(-PI/6)(mito1);
+	mito1 = T([0,2])([-2,6.5])(mito1);
+
+	var mito2 = mitocondrio();
+	mito2 = R([0,2])(-PI/4)(mito2);
+	mito2 = T([0,2])([-4.5,8])(mito2);
+
+	var mito3 = mitocondrio();
+	mito3 = R([0,2])(-PI/3)(mito3);
+	mito3 = T([0,2])([-8,2.5])(mito3);
+
+	var mito4 = mitocondrio();
+	mito4 = R([0,2])(-PI/2)(mito4);
+	mito4 = T([0,2])([6.5,3])(mito4);
+
+	var mito5 = mitocondrio();
+	mito5 = R([0,1])(-PI/4)(mito5);
+	mito5 = T([0,1])([-5.5,-5.5])(mito5);
+
+	var mito6 = mitocondrio();
+	mito6 = R([0,1])(-PI/3)(mito6);
+	mito6 = T([0,1])([7.5,-2.5])(mito6);
+
+	var mito7 = mitocondrio();
+	mito7 = R([0,1])(-PI/2)(mito7);
+	mito7 = T([0,1])([9.5,-1.5])(mito7);
+
+	var mito8 = mitocondrio();
+	mito8 = R([0,1])(-PI/4)(mito8);
+	mito8 = T([0,1])([11.5,-2.3])(mito8);
+
+	var mito9 = mitocondrio();
+	mito9 = R([0,1])(-PI/5)(mito9);
+	mito9 = T([0,1])([20.5,-1.3])(mito9);
+
+
+	var model = STRUCT([mito1,mito2,mito3,mito4,mito5,mito6,mito7,mito8,mito9]);
+	return model;
+}
+
+
+var lisosoma = function(){
+
+	var r = 0.2;
+	var n = 20;
+
+	var sphereDomain = DOMAIN([[0,PI],[0,2*PI]])([n,n]);
+	var sphere = MAP(function(p){
+		var u = p[0]-PI/2;
+		var v = p[1]-PI;
+		return [r*Math.cos(u)*Math.sin(v),r*Math.cos(u)*Math.cos(v),r*Math.sin(u)];
+	})(sphereDomain);
+
+	var lisosoma = COLOR([238/255,173/255,14/255])(sphere);
+	lisosoma = S([0])([1.5])(lisosoma);
+	return lisosoma;
+
+}
+
+var lisosomi = function(){
+	var liso1 = lisosoma();
+	liso1 = R([0,2])(-PI/6)(liso1);
+	liso1 = T([0,2])([-4.5,6.5])(liso1);
+
+
+	var model = STRUCT([liso1]);
+}
+
 
 var nucleo = nucleo();
 var nucleolo = nucleolo();
 var cellula = cellula();
-
+var golgi = complessoGolgi();
+var mitocondri = mitocondri();
 var dendriti = dendriti();
-var neurone = STRUCT([nucleo,nucleolo,cellula,dendriti]);
+var lisosomi = lisosomi();
+var neurone = STRUCT([nucleo,cellula,nucleolo,dendriti,golgi,mitocondri,lisosomi]);
 
-DRAW(neurone)
+
+
+DRAW(neurone);
